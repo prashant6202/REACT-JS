697d9b9e4d5bddd443ea3bed9ec0f8f6018a93f3
Prashant yaduvanahi <122152040+prashant6202@users.noreply.github.com>
2024-03-09 21:49:07 +0530

    Add files via upload

---
+++ REACT_CRA/1.CLASS_COMPONENT/1.myapp1/src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Hello from "./Hello";
+export default class App extends Component {
+  render() {
+    return <Hello />;
+  }
+}
---
+++ REACT_CRA/1.CLASS_COMPONENT/1.myapp1/src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import App from "./App";
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello sir</h1>
+      </div>
+    );
+  }
+}
